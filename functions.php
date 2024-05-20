3e63b4a3bde6cac04c2bbbb10543a3fc01ec5bc2
Rafael Camison <rafaelcamison@gmail.com>
2024-05-21 01:53:52 +0200

    Update asset enqueue versioning in theme functions
    The versioning for CSS and JS files has been updated in the artistry_showcase_scripts function. Now using the file modification time for version controlling, this ensures the most recent asset files are utilized and reduces the risk of serving stale cached files to the users.

--- functions.php
+++ functions.php
@@ -9,26 +9,27 @@
  *
  */
 function artistry_showcase_setup() {
-	load_theme_textdomain('artistry-showcase', get_template_directory() . '/languages');
+	load_theme_textdomain( 'artistry-showcase', get_template_directory() . '/languages' );
 
-	add_theme_support('title-tag');
+	add_theme_support( 'title-tag' );
 
-	add_theme_support('post-thumbnails');
+	add_theme_support( 'post-thumbnails' );
 
-	add_theme_support('html5', array(
+	add_theme_support( 'html5', [
 		'search-form',
 		'comment-form',
 		'comment-list',
 		'gallery',
 		'caption'
-	));
+	] );
 
-	register_nav_menus(array(
-		'main-menu' => __('Main Menu', 'artistry-showcase'),
-		'footer-menu' => __('Footer Menu', 'artistry-showcase')
-	));
+	register_nav_menus( [
+		'main-menu'   => __( 'Main Menu', 'artistry-showcase' ),
+		'footer-menu' => __( 'Footer Menu', 'artistry-showcase' )
+	] );
 }
-add_action('after_setup_theme', 'artistry_showcase_setup');
+
+add_action( 'after_setup_theme', 'artistry_showcase_setup' );
 
 /**
  * Enqueues the styles and scripts for the Artistry Showcase theme.
@@ -40,10 +41,11 @@ add_action('after_setup_theme', 'artistry_showcase_setup');
  *
  */
 function artistry_showcase_scripts() {
-	wp_enqueue_style('artistry-showcase-style', get_stylesheet_uri());
-	wp_enqueue_script('artistry-showcase-scripts', get_template_directory_uri() . '/assets/js/custom-scripts.js', array('jquery'), null, true);
+	wp_enqueue_style( 'artistry-showcase-style', get_stylesheet_uri(), array(), filemtime( get_template_directory() . '/style.css' ) );
+	wp_enqueue_script( 'artistry-showcase-scripts', get_template_directory_uri() . '/assets/js/custom-scripts.js', array( 'jquery' ), filemtime( get_template_directory() . '/assets/js/custom-scripts.js' ), true );
 }
-add_action('wp_enqueue_scripts', 'artistry_showcase_scripts');
+
+add_action( 'wp_enqueue_scripts', 'artistry_showcase_scripts' );
 
 /**
  * Initializes the widgets for the Artistry Showcase theme.
@@ -55,17 +57,18 @@ add_action('wp_enqueue_scripts', 'artistry_showcase_scripts');
  *
  */
 function artistry_showcase_widgets_init() {
-	register_sidebar(array(
-		'name' => __('Sidebar', 'artistry-showcase'),
-		'id' => 'sidebar-1',
-		'description' => __('Add widgets here to appear in your sidebar.', 'artistry-showcase'),
+	register_sidebar( [
+		'name'          => __( 'Sidebar', 'artistry-showcase' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Add widgets here to appear in your sidebar.', 'artistry-showcase' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget' => '</section>',
-		'before_title' => '<h2 class="widget-title">',
-		'after_title' => '</h2>',
-	));
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	] );
 }
-add_action('widgets_init', 'artistry_showcase_widgets_init');
+
+add_action( 'widgets_init', 'artistry_showcase_widgets_init' );
 
 /**
  * Sets up the custom header for the Artistry Showcase theme.
@@ -77,16 +80,17 @@ add_action('widgets_init', 'artistry_showcase_widgets_init');
  *
  */
 function artistry_showcase_custom_header_setup() {
-	add_theme_support('custom-header', apply_filters('artistry_showcase_custom_header_args', array(
+	add_theme_support( 'custom-header', apply_filters( 'artistry_showcase_custom_header_args', array(
 		'default-image' => get_template_directory_uri() . '/assets/images/header.jpg',
-		'width' => 2000,
-		'height' => 1200,
-		'flex-height' => true,
-		'flex-width' => true,
-		'header-text' => false,
-	)));
+		'width'         => 2000,
+		'height'        => 1200,
+		'flex-height'   => true,
+		'flex-width'    => true,
+		'header-text'   => false,
+	) ) );
 }
-add_action('after_setup_theme', 'artistry_showcase_custom_header_setup');
+
+add_action( 'after_setup_theme', 'artistry_showcase_custom_header_setup' );
 
 /**
  * Sets up the custom background for the Artistry Showcase theme.
@@ -98,12 +102,13 @@ add_action('after_setup_theme', 'artistry_showcase_custom_header_setup');
  *
  */
 function artistry_showcase_custom_background_setup() {
-	add_theme_support('custom-background', apply_filters('artistry_showcase_custom_background_args', array(
+	add_theme_support( 'custom-background', apply_filters( 'artistry_showcase_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
-	)));
+	) ) );
 }
-add_action('after_setup_theme', 'artistry_showcase_custom_background_setup');
+
+add_action( 'after_setup_theme', 'artistry_showcase_custom_background_setup' );
 
 /**
  * Registers a custom post type for showcasing portfolios.
@@ -111,39 +116,40 @@ add_action('after_setup_theme', 'artistry_showcase_custom_background_setup');
  * @return void
  */
 function artistry_showcase_custom_post_type() {
-	$labels = array(
-		'name' => __('Portfolios', 'artistry-showcase'),
-		'singular_name' => __('Portfolio', 'artistry-showcase'),
-		'menu_name' => __('Portfolios', 'artistry-showcase'),
-		'name_admin_bar' => __('Portfolio', 'artistry-showcase'),
-		'add_new' => __('Neues Portfolio hinzufügen', 'artistry-showcase'),
-		'add_new_item' => __('Neues Portfolio hinzufügen', 'artistry-showcase'),
-		'new_item' => __('Neues Portfolio', 'artistry-showcase'),
-		'edit_item' => __('Portfolio bearbeiten', 'artistry-showcase'),
-		'view_item' => __('Portfolio anzeigen', 'artistry-showcase'),
-		'all_items' => __('Alle Portfolios', 'artistry-showcase'),
-		'search_items' => __('Portfolios durchsuchen', 'artistry-showcase'),
-		'parent_item_colon' => __('Übergeordnetes Portfolio:', 'artistry-showcase'),
-		'not_found' => __('Keine Portfolios gefunden.', 'artistry-showcase'),
-		'not_found_in_trash' => __('Keine Portfolios im Papierkorb gefunden.', 'artistry-showcase')
-	);
+	$labels = [
+		'name'               => __( 'Portfolios', 'artistry-showcase' ),
+		'singular_name'      => __( 'Portfolio', 'artistry-showcase' ),
+		'menu_name'          => __( 'Portfolios', 'artistry-showcase' ),
+		'name_admin_bar'     => __( 'Portfolio', 'artistry-showcase' ),
+		'add_new'            => __( 'Neues Portfolio hinzufügen', 'artistry-showcase' ),
+		'add_new_item'       => __( 'Neues Portfolio hinzufügen', 'artistry-showcase' ),
+		'new_item'           => __( 'Neues Portfolio', 'artistry-showcase' ),
+		'edit_item'          => __( 'Portfolio bearbeiten', 'artistry-showcase' ),
+		'view_item'          => __( 'Portfolio anzeigen', 'artistry-showcase' ),
+		'all_items'          => __( 'Alle Portfolios', 'artistry-showcase' ),
+		'search_items'       => __( 'Portfolios durchsuchen', 'artistry-showcase' ),
+		'parent_item_colon'  => __( 'Übergeordnetes Portfolio:', 'artistry-showcase' ),
+		'not_found'          => __( 'Keine Portfolios gefunden.', 'artistry-showcase' ),
+		'not_found_in_trash' => __( 'Keine Portfolios im Papierkorb gefunden.', 'artistry-showcase' )
+	];
 
-	$args = array(
-		'labels' => $labels,
-		'public' => true,
+	$args = [
+		'labels'             => $labels,
+		'public'             => true,
 		'publicly_queryable' => true,
-		'show_ui' => true,
-		'show_in_menu' => true,
-		'query_var' => true,
-		'rewrite' => array('slug' => 'portfolio'),
-		'capability_type' => 'post',
-		'has_archive' => true,
-		'hierarchical' => false,
-		'menu_position' => null,
-		'supports' => array('title', 'editor', 'thumbnail', 'excerpt', 'comments'),
-		'show_in_rest' => true
-	);
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => [ 'slug' => 'portfolio' ],
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => null,
+		'supports'           => [ 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ],
+		'show_in_rest'       => true
+	];
 
-	register_post_type('portfolio', $args);
+	register_post_type( 'portfolio', $args );
 }
-add_action('init', 'artistry_showcase_custom_post_type');
+
+add_action( 'init', 'artistry_showcase_custom_post_type' );
